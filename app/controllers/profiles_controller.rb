13db24774b727765c561bf8db4490f372ea44995
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-12 19:34:11 +0000

    Created form for a new profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,6 @@ class ProfilesController < ApplicationController
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @variable = params[:hello]
     @profile = @user.build_profile
   end
 end
