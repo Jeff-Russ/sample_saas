42f63318372c5449f81ab93ee298835618ecadd9
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-12 18:46:22 +0000

    Fixed broken routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :contacts
-  get '/about' => 'pages#about'
-  root 'pages#home'
-  Rails.application.routes.draw do
-    devise_for :users, controllers: { registrations: 'users/registrations' }
-    resources :users do
-      resource :profile
+
+  resources :users do
+    resource :profile
   end
+  
+  resources :contacts
+  
+  get '/about' => 'pages#about'
+  
+  root 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
