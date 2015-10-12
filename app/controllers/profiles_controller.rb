5f6e02c653971097a8b301f7e590fd114f7aa1fa
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-12 20:19:12 +0000

    Added update action for update profile button

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,8 +22,23 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
+    
+  def update
+  end
+  
 end
