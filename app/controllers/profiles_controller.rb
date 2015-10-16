5191e9b0bf932468475a40959d802c68a508b248
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-16 02:38:34 +0000

    Deleted redundent update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -37,8 +37,5 @@ class ProfilesController < ApplicationController
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
-    
-  def update
-  end
   
 end
